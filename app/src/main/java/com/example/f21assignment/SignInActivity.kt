9ee4e8699fef6d41e37eeaa1c15e3106643496f6
sav-ru <60156331+sav-ru@users.noreply.github.com>
2021-11-12 16:09:22 -0500

    Adding the ability to signin to the app, adding the sign in page and the connecting to the database. - allowed to signin via email && google account

---
+++ app/src/main/java/com/example/f21assignment/SignInActivity.kt
@@ -0,0 +1,63 @@
+package com.example.f21assignment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+/**
+ * Resources: https://firebase.google.com/docs/firestore/query-data/listen
+ */
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            //sign in via emails
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            //sign in via google
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            val intent = Intent(this, GridRecyclerViewActivity::class.java)
+            intent.putExtra("user", user)
+            startActivity(intent)
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Toast.makeText(this, "SignIn Failed", Toast.LENGTH_LONG).show()
+            //nav back to the signin activity
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+    }
+}
