7ac203436a39c66e1ac1064330ee6b625e3bf64f
sav-ru <60156331+sav-ru@users.noreply.github.com>
2021-12-07 16:18:49 -0500

    Added functionality to the tool bar on the recycler view page

--- app/src/main/java/com/GC200431861M/f21assignment/GridRecyclerViewActivity.kt
+++ app/src/main/java/com/GC200431861M/f21assignment/GridRecyclerViewActivity.kt
@@ -3,11 +3,15 @@ package com.GC200431861M.f21assignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import com.GC200431861M.f21assignment.databinding.ActivityGridRecyclerViewBinding
 
+
 class GridRecyclerViewActivity : AppCompatActivity(), GridViewAdapter.ChampionItemListener {
     private lateinit var binding : ActivityGridRecyclerViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGridRecyclerViewBinding.inflate(layoutInflater)
@@ -22,11 +26,33 @@ class GridRecyclerViewActivity : AppCompatActivity(), GridViewAdapter.ChampionIt
             binding.gridRecyclerView.adapter = gridViewAdapter
         })
 
-        //when the fab is clicked we want to navigate to the add champion view
-        binding.addChampionFAB.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java));
-        }
+        setSupportActionBar(binding.mainToolBar.toolbar)
     }
+
+    /**
+     * Add the menu to the toolbar
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // when is like switch
+        when(item.itemId){
+            R.id.addChampion -> {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                return true
+            }
+            R.id.championList -> {
+                //disable this be we dont want to load the page again
+                //startActivity(Intent(applicationContext, GridRecyclerViewActivity::class.java))
+                return true
+            }
+        }
+        return  super.onOptionsItemSelected(item)
+    }
+
 
     /**
      * When a champion is selected, pass the Champion information to the ChampionAbilitiesActivity
